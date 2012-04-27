e62392e917839095da521f6ca4197e600e494cf1
Craig Ambrose <craig@craigambrose.com>
2012-04-27 14:20:50 +1200

    stop doing that crazy test routing thing, it breaks main radiant tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-if ENV["RAILS_ENV"] == "test"
-  ActionController::Routing::Routes.draw do |map|
-    map.connect ':controller/:action/:id'
-  end
-end
+# if ENV["RAILS_ENV"] == "test"
+#   ActionController::Routing::Routes.draw do |map|
+#     map.connect ':controller/:action/:id'
+#   end
+# end
